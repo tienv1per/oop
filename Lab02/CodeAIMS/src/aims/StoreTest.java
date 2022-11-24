5d07b8ff11d227ec20e02238df7b43ed06c2aaa0
tienv1per <tienv1perds@gmail.com>
2022-11-24 22:45:41 +0700

    new store file

---
+++ Lab02/CodeAIMS/src/aims/StoreTest.java
@@ -0,0 +1,13 @@
+package aims;
+
+public class StoreTest {
+    public static void main(String[] args){
+        Store StoreDemo = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Geogre Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        StoreDemo.addDVD(dvd1, dvd2, dvd3);
+        StoreDemo.print();
+    }
+}
